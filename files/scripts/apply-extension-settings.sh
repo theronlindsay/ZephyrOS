b441ae3b8eaa17bf0d24428c70d1ebd80e21b176
theronlindsay <theronlindsay@protonmail.com>
2026-02-07 13:42:45 -0700

    Added Vicinae, download page flow update

--- files/scripts/apply-extension-settings.sh
+++ files/scripts/apply-extension-settings.sh
@@ -5,3 +5,6 @@ dconf load /org/gnome/shell/extensions/ < /etc/skel/.config/dconf/extensions.dco
 
 # Apply GNOME shell settings (includes enabled-extensions list)
 dconf load /org/gnome/shell/ < /etc/skel/.config/dconf/shell-settings.dconf || true
+
+# Apply custom keybindings (Alt+Space for Vicinae)
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < /etc/skel/.config/dconf/keybindings.dconf || true
