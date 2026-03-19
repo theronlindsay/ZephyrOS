9da0d9d5aba09f401953607d5dff867857ceb0ef
theronlindsay <theronlindsay@protonmail.com>
2026-03-19 16:52:05 -0600

    Website updates and Updated Fastfetch

---
+++ files/system/usr/etc/profile.d/bazzite-neofetch.sh
@@ -0,0 +1,3 @@
+alias neofetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc'
+alias neowofetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc'
+alias fastfetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc'
